876f0a40a4ffc8855d2c2fc4bfd9d5acd9845a34
freshcodeAlice <freshjs.ofte@gmail.com>
2023-01-13 10:19:01 +0200

    deleteTask back done

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -25,3 +25,25 @@ module.exports.createUserTask = async (req, res, next) => {
         next(error);
     }
 }
+
+
+module.exports.deleteTask = async (req, res, next) => {
+    try {
+        const {params: {taskId}, tokenPayload: {userId}} = req;
+        const foundTask = await Task.findOne({authorId: userId, _id: taskId});
+       
+        if (foundTask) {
+          const deleted = await foundTask.remove();
+            if (deleted) {
+             res.status(200).send(deleted);
+                } else {
+                    res.status(404).send();
+                }
+        } else {
+            res.status(400).send('AuthorId is not correct');
+        }
+        
+    } catch(error) {
+        next(error);
+    }
+}
--- server/routes/task.js
+++ server/routes/task.js
@@ -5,7 +5,7 @@ const taskRouter = Router();
 
 taskRouter.post('/', checkToken, TaskController.createUserTask); 
 taskRouter.get('/', checkToken, TaskController.getAllUserTasks); 
-
+taskRouter.delete('/:taskId', checkToken, TaskController.deleteTask);
 
 module.exports = taskRouter;
 

5105ffaac2289b49e0dd8f1c92fa51cbea583d06
freshcodeAlice <freshjs.ofte@gmail.com>
2023-01-16 11:59:10 +0200

    redux welcome!

--- client/src/Counter.js
+++ client/src/Counter.js
@@ -1,51 +1,45 @@
 import React, {useReducer} from 'react';
+import {connect} from 'react-redux';
 
-const reducer = (state, action) => {
-    switch(action.type) {
-        case 'COUNTER_PLUS': {
-            return {
-                counter: state.counter + 1
-            }
-        }
-        case 'COUNTER_MINUS': {
-            return {
-                counter: state.counter - 1
-            }
-        }
-        default: {
-            return state
-        }
-    }
-}
-
-
-const Counter = () => {
-     const [state, dispatch] = useReducer(reducer, {
-                                            counter: 0
-                                        });
+const Counter = (props) => {
+          
     
     const increment = () => {
         const action = {
             type: 'COUNTER_PLUS'
         }
-        dispatch(action);
+       props.dispatch(action);
     }
 
     const decrement = () => {
         const action = {
             type: 'COUNTER_MINUS'
         }
-        dispatch(action);
+       props.dispatch(action);
     }
     
-    
+    console.log(props);
     return (
         <>
-            <h1>{state.counter}</h1>
+            <h1>{props.counter}</h1>
             <button onClick={increment}>+</button>
             <button onClick={decrement}>-</button>
         </>
     )
 }
 
-export default Counter;
+/*
+Connect - це функція, що приймає два аргументи (обидва опціональні) і підписує компоненту на оновлення стейту
+- mapStateToProps - фунція, що приймає ВЕСЬ стейт (стан додатку) і повертає тільки ту частину стейта, яка потрібна ЦІЙ компоненті
+- mapDispatchToProps
+
+*/
+
+const mapStateToProps = (state) => {
+    return state
+}
+
+const WrappedCounter = connect(mapStateToProps)(Counter);
+
+
+export default WrappedCounter;
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Counter from './Counter';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const initialState = {
+  counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'COUNTER_PLUS': {
+      return {
+        ...state,
+        counter: state.counter + 1
+      }
+    }
+    case 'COUNTER_MINUS': {
+      return {
+        ...state,
+        counter: state.counter - 1
+      }
+    }
+    default: {
+      return state;
+    }
+  }
+
+}
+
+const store = createStore(reducer);
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-   <Counter />
+  <Provider store={store}>
+       <Counter />
+   </Provider>
  //   <App />
 
 );

2320d94b0ee6687940ab9a8c524518edb34a2e98
freshcodeAlice <freshjs.ofte@gmail.com>
2023-01-18 11:55:48 +0200

    devTools enhancer!

---
+++ client/src/Clicker.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Clicker = () => {
+    return (
+        <div style={{width: '100px', height: '100px', backgroundColor: 'red'}}>
+            <h2>Click me</h2>
+        </div>
+    );
+}
+
+export default Clicker;
+
+
+/*
+За натиснення на нього на тестову ручку на сервері має відправитись 
+{
+data: 'hello'
+}
+
+
+*/
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Counter from './Counter';
+import Clicker from './Clicker';
 import store from './store';
 import {Provider} from 'react-redux';
 
@@ -12,7 +13,10 @@ import {Provider} from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+    <>
        <Counter />
+       <Clicker />
+       </>
    </Provider>
  //   <App />
 
